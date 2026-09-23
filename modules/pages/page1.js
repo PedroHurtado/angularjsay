06919cfdb289294864563f2d04b82d542cb60ce1
Pedro Hurtado <pedrohurt@gmail.com>
2026-09-23 16:54:51 +0200

    fetch data and add dom page

--- modules/pages/page1.js
+++ modules/pages/page1.js
@@ -1,3 +1,35 @@
+const outlet = document.getElementById('outlet')
+
+
+function drawData(data){
+    
+    //TemplateSintax
+
+    /*<div class="list" ngfor="let item of items">
+        <div>{{item.id}}</div>
+        <div>{{item.name}}</div>
+    </div>*/
+
+   data.forEach(person=>{
+        const divId = document.createElement('div')
+        divId.textContent = person.id
+
+        const divName = document.createElement('div')
+        divName.textContent = person.name
+
+        const divContainer = document.createElement('div')
+        divContainer.className = "list"
+        divContainer.appendChild(divId)
+        divContainer.appendChild(divName)
+        outlet.appendChild(divContainer)
+   })
+}
+async function getData(){
+    //http
+    const response = await fetch("data.json");
+    drawData(await response.json())
+}
+
 export default function pagina1(){
-    console.log('pagina1')
+    getData();
 }
